b4d54618abfc1768c088ca121eb39e4c674135ab
JohnSmith114 <kientuong@gmail.com>
2019-05-04 01:48:09 +0200

    Reading environmental params to get keys instead of reading local secret

--- tweeter.py
+++ tweeter.py
@@ -1,7 +1,15 @@
 from twython import Twython
 import tweetGenerator
+import os
 
-consumer_key, consumer_secret, access_token, access_token_secret = [line.strip('\n') for line in open("auth.secret")]
+if os.path.isfile("auth.secret"):
+    consumer_key, consumer_secret, access_token, access_token_secret = [line.strip('\n') for line in open("auth.secret")]
+else:
+    consumer_key = os.environ['CONSUMER_KEY']
+    consumer_secret = os.environ['CONSUMER_SECRET']
+    access_token = os.environ['ACCESS_TOKEN']
+    access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
+
 
 twitter = Twython(consumer_key, consumer_secret, access_token, access_token_secret)
 
